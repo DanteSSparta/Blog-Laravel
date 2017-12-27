e543f3f6522eebb95c03d3638d191763155f0370
DanteSSparta <vladimir_vedmedev@mail.ru>
2017-12-27 17:55:07 +0200

    Add files via upload

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.master')
+
+@section('content')
+	<div class="col-sm-8 blog-main">
+
+		<div class="blog-post">
+            <h1 class="blog-post-title">
+                {{$post->title}}
+            </h1>
+            <p class="blog-post-meta">
+              {{$post->created_at->toFormattedDateString()}}
+            </p>
+
+            @if(count($post->tags))
+                <ul>
+                    @foreach ($post->tags as $tag)
+                        <li>
+                            <a href="/posts/tags/{{$tag->name}}">
+                            {{$tag->name}}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            @endif
+
+            {{$post->body}}
+
+          	<hr>
+
+            <div class="comments">
+            	<ul class="list-group"> 
+            	@foreach ($post->comments as $comment)
+            		<li class="list-group-item">
+            			<strong>
+            				{{$comment->created_at->diffForHumans() }} : &nbsp;
+            			</strong>
+            			{{ $comment->body}}
+            		</li>
+            	@endforeach
+            	</ul>
+            </div>
+
+            <hr>
+
+            <div class="card">
+            	<div class="card-block">
+            		<form method="POST" action="/posts/{{$post->id}}/comments/">
+            			{{csrf_field()}}
+	            		<div class="form-group">
+							<textarea name="body" placeholder="Your comment here." class="form-control"></textarea>
+						</div>
+
+						<div class="form-group">
+							<button type="submit" class="btn btn-primary">Add Comment</button>
+						</div>	
+					</form>
+            	</div>
+            </div>
+
+            @include ('layouts.errors')
+          </div><!-- /.blog-post -->
+	</div>
+@endsection
